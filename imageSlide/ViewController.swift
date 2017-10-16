e18e6f97a43591c679ba071aa61d56ce9806dbae
nenu <nino_roldan@yahoo.com>
2017-10-16 22:36:35 +0900

    created picture name array and array placement.

--- imageSlide/ViewController.swift
+++ imageSlide/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    let picArray:[String] = ["image1.jpg", "image2.jpg", "image3.jpg"]
+    var i:Int = 0 //variable that holds array placement
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
